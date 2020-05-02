6dfd8455c1b66831409188a04d8a43c3f7fcff72
blasco <blascoburguillos@gmail.com>
2020-05-02 13:11:25 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 4200,
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: 'src/index.html', to: 'index.html' },
+            { from: 'src/assets', to: 'assets/' },
+        ])
+    ],
+};
